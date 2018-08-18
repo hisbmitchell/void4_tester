533f58cc05f2a411804c9ad56f733058dbbda6c3
hisbmitchell <heathermitchell.ottawa@gmail.com>
2018-08-17 22:51:56 -0400

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,15 @@
+function setup() {
+   createCanvas(windowWidth, windowHeight);
+   background('#bc0319');
+   
+}
+
+function draw() {
+  background('#bc0319');
+  
+}
+
+ function windowResized(){
+        resizeCanvas(windowWidth, windowHeight);
+        //bgVideo.size(windowHeight*2, 300);
+      }
